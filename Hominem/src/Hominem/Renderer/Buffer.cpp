bfc4512c861a7b965651a931e085162becd467e1
Owner <vicyonjou@gmail.com>
2025-08-27 21:09:01 -0400

    feat: add basic 2D Quad Renderer

--- Hominem/src/Hominem/Renderer/Buffer.cpp
+++ Hominem/src/Hominem/Renderer/Buffer.cpp
@@ -6,12 +6,12 @@
 
 namespace Hominem {
 
-	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None: HMN_CORE_ASSERT(false, "RendererAPI::None is currently not supported") return nullptr;
-			case RendererAPI::API::OpenGL: return new OpenGLVertexBuffer(vertices, size);
+			case RendererAPI::API::OpenGL: return std::make_shared<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		HMN_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -19,12 +19,12 @@ namespace Hominem {
 	}
 
 
-	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None: HMN_CORE_ASSERT(false, "RendererAPI::None is currently not supported") return nullptr;
-			case RendererAPI::API::OpenGL: return new OpenGLIndexBuffer(indices, size);
+			case RendererAPI::API::OpenGL: return std::make_shared<OpenGLIndexBuffer>(indices, size);
 		}
 
 		HMN_CORE_ASSERT(false, "Unknown RendererAPI!");
--- Hominem/src/Hominem/Renderer/Buffer.h
+++ Hominem/src/Hominem/Renderer/Buffer.h
@@ -130,7 +130,7 @@ namespace Hominem {
 		virtual void SetLayout(const BufferLayout& layout) = 0;
 		virtual const BufferLayout& GetLayout() const = 0;
 
-		static VertexBuffer* Create(float* vertices, uint32_t size);
+		static Ref<VertexBuffer> Create(float* vertices, uint32_t size);
 	};
 
 	class IndexBuffer
@@ -144,6 +144,6 @@ namespace Hominem {
 
 		virtual uint32_t GetCount() const = 0;
 
-		static IndexBuffer* Create(uint32_t* indices, uint32_t size);
+		static Ref<IndexBuffer> Create(uint32_t* indices, uint32_t size);
 	};
 }
