de1cdd7f309b5196e5ed51bdde6a6bbe91d24fd3
Owner <vicyonjou@gmail.com>
2025-08-30 11:10:55 -0400

    feat: enable textures and transform for 2D rendering

--- Hominem/src/Hominem/Renderer/Renderer2D.h
+++ Hominem/src/Hominem/Renderer/Renderer2D.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "Camera.h"
+#include "Texture.h"
 
 namespace Hominem {
 
 	class Renderer2D
 	{
 	public: 
-		// Core Rendering
 		static void Init();
 		static void Shutdown();
 
@@ -16,9 +16,8 @@ namespace Hominem {
 
 		// Primitives
 		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
-		
-		//z-axis can be used for depth features
-		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
-
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color); //z-axis in the position can be used for depth features
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture);
 	};
 }
