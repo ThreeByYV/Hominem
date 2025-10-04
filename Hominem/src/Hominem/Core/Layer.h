4399301ae157b3dae02e783fc3318c3864c5cc7f
Owner <vicyonjou@gmail.com>
2025-10-04 14:35:17 -0400

    feat: layers for handling app states

--- Hominem/src/Hominem/Core/Layer.h
+++ Hominem/src/Hominem/Core/Layer.h
@@ -10,18 +10,28 @@ namespace Hominem {
 	{
 	public:
 		Layer(const std::string& name = "Layer");
-		virtual ~Layer();
+		virtual ~Layer() = default;
 
 		virtual void OnAttach() {}
 		virtual void OnDetach() {}
+
 		virtual void OnUpdate(Timestep ts) {}
 		virtual void OnImGuiRender() {}
+
 		virtual void OnEvent(Event& event) {}
 
-		inline const std::string& GetName() const { return m_DebugName;  }
+		inline const std::string& GetName() const { return m_DebugName; }
+
+		template<std::derived_from<Layer> T, typename...Args>
+		void TransitionTo(Args&&... args)
+		{
+			QueueTransition(std::move(std::make_unique<T>(std::forward<Args>(args)...)));
+		}
 
 	protected:
 		std::string m_DebugName;
 
+	private:
+		void QueueTransition(std::unique_ptr<Layer> layer);
 	};
 } 
--- Hominem/src/Hominem/Core/LayerStack.h
+++ Hominem/src/Hominem/Core/LayerStack.h
@@ -8,21 +8,19 @@ namespace Hominem {
 	{
 	public:
 		LayerStack();
-		~LayerStack();
+		~LayerStack() = default;
 
-		void PushLayer(Layer* layer);
-		void PushOverlay(Layer* overlay);
+		void PushLayer(std::unique_ptr<Layer> layer);      
+		void PushOverlay(std::unique_ptr<Layer> overlay); 
 		void PopLayer(Layer* layer);
 		void PopOverlayer(Layer* overlay);
 
 
 		//todo add reverse iterator, since will need to propagate events in a reverse fashion
-
-		std::vector<Layer*>::iterator begin() { return m_Layers.begin(); }
-		std::vector<Layer*>::iterator end() { return m_Layers.end(); }
-
+		std::vector<std::unique_ptr<Layer>>::iterator begin() { return m_Layers.begin(); }
+		std::vector<std::unique_ptr<Layer>>::iterator end() { return m_Layers.end(); }
 	private:
-		std::vector<Layer*> m_Layers;
+		std::vector<std::unique_ptr<Layer>> m_Layers;
 		unsigned int m_LayerInsertIndex = 0;
 	};
 
